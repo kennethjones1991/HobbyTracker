f70285bc72497bff7a0eb8ef62c8ec9c278528cb
Kenneth Jones <kennethjones1991@outlook.com>
2020-04-09 14:39:37 -0600

    Initial commit

---
+++ HobbyTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HobbyTracker
+//
+//  Created by Kenneth Jones on 4/9/20.
+//  Copyright © 2020 Kenneth Jones. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
